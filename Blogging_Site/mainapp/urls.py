e4db3105c9cf83ccb641180c0ec132508f62674e
sana0704 <sanandaroy874@gmail.com>
2025-06-07 21:07:21 +0530

    delete/edit files added

--- Blogging_Site/mainapp/urls.py
+++ Blogging_Site/mainapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import HomeView, AddPost
+from .views import HomeView, AddPost ,EditPost,DeletePost
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name = 'homepage'),
 
     path('blog/add', AddPost.as_view(), name = 'create_blog'),
-    # path('blog/add', addPost, name = 'create_blog')
+    path('blog/edit/<int:pk>', EditPost.as_view(),name= 'edit_blog'),
+    path('blog/delete/<int:pk>',DeletePost.as_view(),name='delete_blog'),
+    
     
 ]
--- Blogging_Site/mainapp/views.py
+++ Blogging_Site/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView ,UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
 
@@ -21,10 +21,22 @@ class AddPost(LoginRequiredMixin, CreateView):
     form_class = PostForm
     success_url = '/'
 
+
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
+class EditPost(LoginRequiredMixin,UpdateView):
+    model=Post
+    template_name = 'mainapp/edit_blog.html'
+    form_class = PostForm
+    success_url = '/'
+
+class DeletePost(LoginRequiredMixin,DeleteView):
+    model = Post
+    template_name = 'mainapp/delete_blog.html'
+    success_url = '/'
 # def addPost(request):
 #     if request.method == 'GET':
 #         form = PostForm()
